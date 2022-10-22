cf3bd496109a040caf1fbc02623e2cb0fa5bce01
qu1z5 <Yariksaij@gmail.com>
2022-10-22 13:25:01 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,107 @@
+import random
+import logging
+import requests
+import os
+import shutil
+
+from dotenv import load_dotenv
+
+IMAGES_DIR = 'images'
+
+
+def upload_pic_to_the_wall(alt, user_id, access_token, media_id):
+    params = {
+        'access_token': access_token,
+        'owner_id': f'-{group_id}',
+        'v': 5.131,
+        'from_group': 1,
+        'message': alt,
+        'attachments': f'photo{user_id}_{media_id}'
+    }
+    url = 'https://api.vk.com/method/wall.post'
+    response = requests.post(url, params=params)
+    response.raise_for_status()
+
+
+def upload_pic(access_token, group_id, photo_params, photo_hash, photo_server):
+    params = {
+        'access_token': access_token,
+        'group_id': group_id,
+        'photo': photo_params,
+        'hash': photo_hash,
+        'server': photo_server,
+        'v': 5.131
+    }
+    url = 'https://api.vk.com/method/photos.saveWallPhoto'
+    response = requests.post(url, params=params)
+    response.raise_for_status()
+    media_id = response.json()['response'][0]['id']
+    return media_id
+
+
+def upload_pic_on_server(upload_url):
+    with open(os.path.join('images', 'pc.jpg'),'rb') as file:
+        files = {
+            'photo': file
+        }
+        url = upload_url
+        response = requests.post(url, files=files)
+        response.raise_for_status()
+    photo_params = response.json()['photo']
+    photo_hash = response.json()['hash']
+    photo_server = response.json()['server']
+    return photo_params, photo_hash, photo_server
+
+def get_upload_link(access_token):
+    params = {
+        'group_id': group_id,
+        'access_token': access_token,
+        'v': 5.131
+    }
+    url = 'https://api.vk.com/method/photos.getWallUploadServer'
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    upload_url = response.json()['response']['upload_url']
+    return upload_url
+
+def search_comic():
+    url = 'https://xkcd.com/info.0.json'
+    response = requests.get(url)
+    response.raise_for_status()
+    num = response.json()['num']
+    url = f'https://xkcd.com/{random.randint(1, num)}/info.0.json'
+    response = requests.get(url)
+    img = response.json()['img']
+    alt = response.json()['alt']
+    return img, alt
+
+def download_image(img, path,  params=''):
+    response = requests.get(img, params=params)
+    response.raise_for_status()
+    with open(path, 'wb') as file:
+        file.write(response.content)
+
+if __name__ == '__main__':
+    load_dotenv()
+    group_id = os.getenv('GROUP_ID')
+    access_token = os.getenv('ACCESS_TOKEN')
+    client_id = os.getenv('CLIENT_ID')
+    user_id = os.getenv('USER_ID')
+    image_name = 'pc.jpg'
+    os.makedirs(IMAGES_DIR, exist_ok=True)
+    path = os.path.join(IMAGES_DIR, image_name)
+    try:
+        img, alt = search_comic()
+        download_image(img, path, params='')
+        get_upload_link(access_token)
+        upload_url = get_upload_link(access_token)
+        upload_pic_on_server(upload_url)
+        photo_params, photo_hash, photo_server = upload_pic_on_server(upload_url)
+        media_id = upload_pic(access_token, group_id, photo_params, photo_hash, photo_server)
+        upload_pic(access_token, group_id, photo_params, photo_hash, photo_server)
+        upload_pic_to_the_wall(alt, user_id, access_token, media_id)
+    except requests.exceptions.HTTPError:
+        logging.exception()
+    finally:
+        shutil.rmtree('images')
+
